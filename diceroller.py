91b89a2847c2fc4ba5f1799a37223bbdf7d0ec7c
Marcus <itsZeroday97@gmail.com>
2022-10-11 01:03:43 +0000

    Rough draft.

--- diceroller.py
+++ diceroller.py
@@ -1,10 +1,9 @@
 import random
-
-print('Enter the number of sides for the die')
-sides = int(input(''))
-print('How many times do you want to roll?')
-rolls = int(input(''))
-
-for x in range(rolls):
-	print('You rolled a...')
-	print(random.randint(1,sides))
+while True:
+	try:
+		sides = int(input('Please enter the number of sides for the die: '))
+		rolls = int(input('How many times do you want to roll: '))
+		for x in range(rolls):
+			print(random.randint(1,int(sides)))
+	except ValueError:
+		print('Sorry, that is an invalid input. Try again...')
